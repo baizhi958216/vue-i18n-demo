c472670b9872bf0d4660102cbfe77a4514768314
baizhi958216 <1475289190@qq.com>
2024-05-29 11:29:29 +0800

    feat: 新增翻译

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,25 +10,25 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count++">{{ $t('message.count') }} {{ count }}</button>
     <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      {{ $t('message.edit') }}
+      <code>components/HelloWorld.vue</code> {{ $t('message.testhmr') }}
     </p>
   </div>
 
   <p>
-    Check out
+    {{ $t('message.checkout') }}
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
-    >, the official Vue + Vite starter
+    >, {{ $t('message.officialstarter') }}
   </p>
   <p>
-    Install
+    {{ $t('message.install') }}
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
+    {{ $t('message.volarinstall') }}
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">{{ $t('message.learnmore') }}</p>
 </template>
 
 <style scoped>
